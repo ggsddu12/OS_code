aa28aa4750dfd96166d706ef1b0160c6eed005e1
ggsddu_12 <ggsddu_86@proton.me>
2022-11-01 21:40:32 +0800

    内存检测

--- master/src/loader.asm
+++ master/src/loader.asm
@@ -2,10 +2,35 @@
 
 dw 0x55aa ;魔数，用于判断错误
 
-xchg bx, bx
 
 mov si, booting
 call print
+
+;内存检查
+dectecting_memory:
+    ;int 0x15 设置入参
+    xor ebx, ebx ; ebx清空
+    mov ax, 0
+    mov es, ax
+    mov edi, ards_buffer
+    mov edx, 0x534d4150
+.next
+    mov ecx, 20  ; ards大小
+    
+    mov eax, 0xe820   ;bios内存检查的子功能号
+    int 0x15     ; bios系统调用
+    
+    ;返回值
+    jc error     ; CF=1,出错
+    add di, cx   ; ards结构体指针后移
+    inc word [ards_count] ; ards数量加1
+    cmp ebx, 0   ; 检查结束
+    jnz .next    ; 继续检查
+    
+    mov si, dectecting
+    call print   ;打印检查成功信息
+
+
 
 
 jmp $
@@ -23,3 +48,17 @@ print:
     ret
 booting:
     db "Loading Onix...", 10 ,13 ,0 ;\n\r
+dectecting:
+    db "Dectecting memory success...", 10, 13, 0 
+
+error:
+    mov si, .msg
+    call print
+    hlt
+    jmp $
+    .msg db "Loading error!!!", 10 , 13 0
+
+ards_count:
+    dw 0
+ards_buffer:
+
